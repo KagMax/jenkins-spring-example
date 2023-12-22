9af3cfb7d54314d8a265ea9977fd79e7728396f9
SdaJava <zakharovmaksym@gmail.com>
2023-12-22 18:16:04 +0100

    Initial commit

---
+++ src/test/java/com/example/jenkinsspringexample/JenkinsSpringExampleApplicationTests.java
@@ -0,0 +1,33 @@
+package com.example.jenkinsspringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class JenkinsSpringExampleApplicationTests {
+
+    @Autowired
+     private  MockMvc mockMvc;
+
+    @Test
+    void shouldReturnDefaultMessage() throws Exception {
+
+
+            mockMvc.perform(MockMvcRequestBuilders.get("/your-endpoint"))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().string("Elowuinw"));
+
+    }
+
+
+
+}
